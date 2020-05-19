e0dcfdabb78001f82e66649220bdaceace93e847
nibble0101 <mj.mawa@yahoo.com>
2020-05-19 23:05:47 +0300

    First set of solutions

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Rosetta Code: Count occurrences of a substring
+
+function countSubstring(str, subStr) {
+    const a = str.length;
+    const b = (str.split(subStr)).join('').length;
+    return (a - b)/subStr.length;
+  }
+  
+//Rosetta Code: Letter frequency
+
+function letterFrequency(txt) {
+    const arr = txt.split(''), obj = {};
+    arr.forEach(val => {
+      if(obj[val]){
+         obj[val]++;
+      }else{
+        obj[val] = 1;
+      }
+    })
+    return Object.entries(obj).sort();
+  }
+//Rosetta Code: Dot product
+  function dotProduct(ary1, ary2) {
+    const product = ary1.reduce((prev, curr, ind) => {
+        return prev  + curr * ary2[ind]  
+    }, 0);
+  
+    return product
+  }
+  
